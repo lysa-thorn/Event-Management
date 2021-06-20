1d29843150b4fbb1becee46e0e858cfddc9745c0
lysa-thorn <lysa.thorn@gmail.com>
2021-06-20 16:14:16 +0700

    Structure project

---
+++ event-api-service/src/test/java/com/example/eventapiservice/EventApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ event-ui/src/main/java/com/example/eventui/EventUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventUiApplication.class, args);
+    }
+
+}
